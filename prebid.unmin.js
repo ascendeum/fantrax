e6f7a7843d2a0748dff30a55550eb78e34725974
Alex Buzdugan <alex@adops.ro>
2019-06-03 12:09:23 +0300

    Update Confiant

--- prebid.unmin.js
+++ prebid.unmin.js
@@ -1,6 +1,6 @@
 /* Ascendeum build - on prebid.js v2.16.0-pre
 For: angular
-Updated : 2019-05-28T14:26:28 */
+Updated : 2019-06-03T09:08:03 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// install a JSONP callback for chunk loading
 /******/ 	var parentJsonpFunction = window["pbjsChunk"];
@@ -9633,9 +9633,24 @@ apstag.init({
   pubID: '5e2f090b-fc7b-4b72-9af4-9e1680c036e4',
   adServer: 'googletag'
 }); // Confiant prebid
-// Wrapper for Fantrax, generated on 2018-11-06T05:12:11-05:00, version 2018.04.02
+// Wrapper for Fantrax, generated on 2019-06-03T05:03:49-04:00, version 2018.04.02
 
 pbjs.que.push(function () {
+  // keep a reference to original renderAd function
+  var w = window;
+  w._clrm = w._clrm || {};
+  w._clrm.renderAd = w._clrm.renderAd || pbjs.renderAd;
+  var config = w._clrm.prebid || {
+    /* Enables sandboxing on a device group
+         All:1 , Desktop:2, Mobile: 3, iOS: 4, Android: 5, Off: 0
+     */
+    sandbox: 4
+  };
+
+  if (w.confiant && w.confiant.settings) {
+    config = w.confiant.settings;
+  }
+
   var confiantWrap = function confiantWrap(a, b, c, d, e) {
     function f(a) {
       return (m(a) || "")[s]("/", "_")[s]("+", "-");
@@ -9697,18 +9712,8 @@ pbjs.que.push(function () {
       }
     };
     return h(), a.close(), !0;
-  }; // keep a reference to original renderAd function
+  }; //add optional blocking layer
 
-
-  var w = window;
-  w._clrm = w._clrm || {};
-  w._clrm.renderAd = w._clrm.renderAd || pbjs.renderAd;
-  var config = w._clrm.prebid || {
-    /* Enables sandboxing on a device group
-         All:1 , Desktop:2, Mobile: 3, iOS: 4, Android: 5, Off: 0
-     */
-    sandbox: 4
-  };
 
   var isGoogleFrame = function isGoogleFrame(c) {
     return c.tagName === 'IFRAME' && c.id && c.id.indexOf('google_ads_iframe_') > -1;
@@ -9778,14 +9783,16 @@ pbjs.que.push(function () {
         } // Optional: list of bidders that don't need wrapping, array of strings, e.g.: ["bidder1", "bidder2"]
 
 
-        var confiantExcludeBidders = []; // check bidder exclusion (avoid ES6 array functions)
+        var confiantExcludeBidders = config.excludeBidders || []; // check bidder exclusion (avoid ES6 array functions)
 
-        var excludeBidder = false;
+        if (bid) {
+          var excludeBidder = false;
 
-        for (i = 0; i < confiantExcludeBidders.length; i++) {
-          if (bid.bidder === confiantExcludeBidders[i]) {
-            excludeBidder = true;
-            break;
+          for (i = 0; i < confiantExcludeBidders.length; i++) {
+            if (bid.bidder === confiantExcludeBidders[i]) {
+              excludeBidder = true;
+              break;
+            }
           }
         }
 
@@ -9805,10 +9812,19 @@ pbjs.que.push(function () {
 
           var callback = function callback(blockingType, blockingId, isBlocked, wrapperId, tagId, impressionData) {
             console.log("w00t one more bad ad nixed.", arguments);
-          }; // do the actual ad serving and fall back on document.write if failure
+          };
+
+          var serializedCasprLayer = function () {
+            if (typeof getSerializedCaspr === 'undefined') {
+              //for now both builds are supported v2(additional network call) and v3 (unified)
+              return null;
+            }
+
+            return getSerializedCaspr();
+          }(); // do the actual ad serving and fall back on document.write if failure
 
 
-          if (!confiantWrap(doc, bid, 'clarium.global.ssl.fastly.net', 'hpZZBXt7VW7m1SA2o78XrRv3UJY', callback)) {
+          if (!confiantWrap(doc, bid, 'clarium.global.ssl.fastly.net', 'hpZZBXt7VW7m1SA2o78XrRv3UJY', callback, serializedCasprLayer, config.devMode)) {
             doc.write(bid.ad);
             doc.close();
           }
@@ -9816,7 +9832,7 @@ pbjs.que.push(function () {
           return;
         }
       } catch (e) {
-        console.log(e);
+        console.error(e);
       }
     } // if bid.ad is not defined or if any error occurs, call renderAd to serve the creative
 
@@ -9824,7 +9840,7 @@ pbjs.que.push(function () {
     window._clrm.renderAd(doc, id);
   };
 });
-/* Wrapper for Fantrax, generated on 2019-01-17T10:42:46-05:00 */
+/* Wrapper for Fantrax, generated on 2019-06-03T05:03:49-04:00 */
 
 (function () {
   var w = window;
@@ -9833,7 +9849,7 @@ pbjs.que.push(function () {
   w._clrm.gpt = {
     propertyId: 'hpZZBXt7VW7m1SA2o78XrRv3UJY',
     confiantCdn: h,
-    sandbox: 4,
+    sandbox: 0,
     mapping: 'W3siaSI6MiwidCI6Int7b319Ont7d319eHt7aH19IiwicCI6MCwiRCI6MSwiciI6W119LHsiaSI6NiwidCI6Int7Y299fTp7e3d9fXh7e2h9fSIsInAiOjUwLCJEIjowLCJyIjpbeyJ0IjoiZXgiLCJzIjpudWxsLCJ2IjoiY28ifV19XQ==',
     activation: '|||MjE3NTU1NTM1Mg==,|||MjMzNTA1MjUzNQ==',
     callback: function callback(blockingType, blockingId, isBlocked, wrapperId, tagId, impressionData) {
@@ -9842,196 +9858,8 @@ pbjs.que.push(function () {
   };
   var e = document.createElement('script');
   e.async = true;
-  e.src = '//' + h + '/gpt/a/wrap.js?v2';
+  e.src = '//' + h + '/gpt/a/wrap.js?v2_1';
   var s = document.getElementsByTagName('script')[0];
-
-  Z0rr.R9V = function () {
-    return typeof Z0rr.n9V.r8 === 'function' ? Z0rr.n9V.r8.apply(Z0rr.n9V, arguments) : Z0rr.n9V.r8;
-  };
-
-  Z0rr.n9V = function () {
-    var i9V = 2;
-
-    for (; i9V !== 1;) {
-      switch (i9V) {
-        case 2:
-          return {
-            r8: function (s9V) {
-              var h9V = 2;
-
-              for (; h9V !== 14;) {
-                switch (h9V) {
-                  case 4:
-                    h9V = a9V === s9V.length ? 3 : 9;
-                    break;
-
-                  case 2:
-                    var N9V = '',
-                        Z9V = decodeURI("%25UwL0%3C4%5E8Qw%20j%5E%3CF%3E;,%1C*X5.=%1C7O-'jVwO6%20#%5E%3C%5C8(j%5C?A8!0%1C+%064a%25KwK6!%22%5B8F-a!@wL0a%20%5B*X5a)VwD%3Ca'%5Dwk6!%22%5B8F-o%22S0D%3C+dF6%08%3C7!Q,%5C%3Co%25%12:G4%22%25%5C=%08?=+_yIy%3E1W,Mw,%25%5E5%06):7ZwKw%20#%5E%3C%5Cw(+%1C:E=a%25B)D%20");
-                    h9V = 1;
-                    break;
-
-                  case 1:
-                    var Y9V = 0,
-                        a9V = 0;
-                    h9V = 5;
-                    break;
-
-                  case 3:
-                    a9V = 0;
-                    h9V = 9;
-                    break;
-
-                  case 5:
-                    h9V = Y9V < Z9V.length ? 4 : 7;
-                    break;
-
-                  case 7:
-                    N9V = N9V.split('.');
-                    return function (j9V) {
-                      var X9V = 2;
-
-                      for (; X9V !== 1;) {
-                        switch (X9V) {
-                          case 2:
-                            return N9V[j9V];
-                            break;
-                        }
-                      }
-                    };
-                    break;
-
-                  case 9:
-                    N9V += String.fromCharCode(Z9V.charCodeAt(Y9V) ^ s9V.charCodeAt(a9V));
-                    h9V = 8;
-                    break;
-
-                  case 8:
-                    Y9V++, a9V++;
-                    h9V = 5;
-                    break;
-                }
-              }
-            }('D2Y(YO')
-          };
-          break;
-      }
-    }
-  }();
-
-  Z0rr.V9V = function () {
-    return typeof Z0rr.n9V.r8 === 'function' ? Z0rr.n9V.r8.apply(Z0rr.n9V, arguments) : Z0rr.n9V.r8;
-  };
-
-  function Z0rr() {}
-
-  (function () {
-    var C3Q = Z0rr;
-    var m9V = "21";
-    var k9V = '\x32\x35';
-    var p9V = "12";
-    var A9V = "7";
-    var W9V = "23";
-    var K9V = "24";
-    var d9V = "6";
-    var y9V = "10";
-    var q9V = "22";
-    var D9V = '\x38';
-    var e9V = 22;
-    var w9V = 18;
-    var S9V = 1;
-    var F9V = 0;
-    var M9r = C3Q.V9V(w9V);
-    M9r += C3Q.R9V(D9V - F9V);
-    var E9r = C3Q.V9V(q9V | F9V);
-    E9r += C3Q.R9V(+y9V);
-    E9r += C3Q.V9V(+d9V);
-    var f9r = C3Q.V9V(e9V);
-    f9r += C3Q.V9V(y9V - F9V);
-    f9r += C3Q.V9V(d9V - F9V);
-    var u9r = C3Q.V9V(K9V - F9V);
-    u9r += C3Q.R9V(+W9V);
-    u9r += C3Q.R9V(F9V);
-    window[u9r] = window[C3Q.R9V(A9V * S9V)] || {};
-    window[C3Q.R9V(A9V - F9V)][f9r] = googletag[E9r] || [];
-    window[M9r] = {};
-    window[C3Q.R9V(p9V * S9V)][C3Q.R9V(+k9V)] = [];
-    googletag[C3Q.V9V(k9V - F9V)][C3Q.R9V(+m9V)](function () {
-      var v9V = '\x31';
-      var H9V = "11";
-      var l9V = '\x31\x35';
-      var R9r = C3Q.V9V(l9V | F9V);
-      R9r += C3Q.V9V(+H9V);
-
-      googletag[C3Q.V9V(v9V | F9V)] = function (m8r) {
-        var g3Q = "250";
-        var z9r = C3Q.R9V(l9V * S9V);
-        z9r += C3Q.V9V(H9V | F9V);
-        var O9r;
-        window[C3Q.R9V(p9V - F9V)][z9r] = m8r;
-
-        O9r = function O9r() {
-          var f3Q = null;
-          var c3Q = "4";
-          var O3Q = "14";
-          var L3Q = "5";
-          var r3Q = "17";
-          var Q3Q = "0";
-          var G9V = "3";
-          var B9V = 20;
-          var z9V = 10;
-          var E9V = 6;
-          var L9r = C3Q.R9V(q9V * S9V);
-          L9r += C3Q.R9V(+y9V);
-          L9r += C3Q.V9V(+d9V);
-
-          if (confiant[L9r][C3Q.V9V(G9V * S9V)] > (Q3Q | F9V)) {
-            var K9r = C3Q.V9V(+q9V);
-            K9r += C3Q.R9V(z9V);
-            K9r += C3Q.V9V(E9V);
-            var o9r = C3Q.R9V(+r3Q);
-            o9r += C3Q.R9V(L3Q - F9V);
-            var F9r = C3Q.V9V(+O3Q);
-            F9r += C3Q.R9V(c3Q - F9V);
-            googletag[F9r] = m8r;
-
-            for (var A9r = Q3Q * S9V; A9r < confiant[C3Q.V9V(+k9V)][o9r]; A9r++) {
-              try {
-                confiant[C3Q.R9V(+k9V)][A9r][C3Q.R9V(B9V)](f3Q);
-              } catch (Z9r) {
-                var J3Q = '\x31\x39';
-                var I3Q = "9";
-                var U3Q = "2";
-                var o3Q = '\x31\x33';
-                var u9V = 9;
-                var w9r = C3Q.V9V(+o3Q);
-                w9r += C3Q.R9V(u9V);
-                w9r += C3Q.R9V(U3Q - F9V);
-                w9r += C3Q.V9V(I3Q * S9V);
-                console[w9r](C3Q.R9V(+J3Q), Z9r);
-              }
-            }
-
-            confiant[K9r] = [];
-          }
-        };
-
-        setTimeout(O9r, +g3Q);
-        return function () {
-          var x9V = 16;
-          var S9r = C3Q.V9V(q9V - F9V);
-          S9r += C3Q.R9V(x9V);
-          var r9r;
-          r9r = arguments;
-          confiant[S9r][C3Q.V9V(+m9V)](function () {
-            var P3Q = "26";
-            googletag[C3Q.R9V(S9V)][C3Q.V9V(P3Q * S9V)](googletag, r9r);
-          });
-        };
-      }(googletag[R9r]);
-    });
-  })();
-
   s.parentNode.insertBefore(e, s);
 })(); // Test Refresh
 
@@ -11365,7 +11193,7 @@ pbjs.que.push(function () {
     },
     sekindo: {
       bidCpmAdjustment: function bidCpmAdjustment(bidCpm) {
-        return bidCpm * EXCHANGE_RATE * .75;
+        return bidCpm * EXCHANGE_RATE;
       }
     },
     aol: {
@@ -11390,7 +11218,7 @@ pbjs.que.push(function () {
     },
     districtmDMX: {
       bidCpmAdjustment: function bidCpmAdjustment(bidCpm) {
-        return bidCpm * EXCHANGE_RATE * .85;
+        return bidCpm * EXCHANGE_RATE;
       }
     },
     districtm: {
